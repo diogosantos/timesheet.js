2b343ac3fe62ea5ee5bfe6afea79f7cce2f9dfcf
Diogo Santos <diosantos@ebay.com>
2016-09-16 12:26:36 +0200

    modifying the component to use javascript dates, instead of years and months.

--- test/timesheet.js
+++ test/timesheet.js
@@ -9,20 +9,7 @@ suite('Timesheet', function() {
   test('Calculation', function(done) {
     var TS = new window.Timesheet();
 
-    assert.equal(12,  (TS.createBubble(60, 2012, TS.parseDate('2002'), TS.parseDate('2002'))).getMonths());
-    assert.equal(12,  (TS.createBubble(60, 2012, TS.parseDate('2002'), TS.parseDate('2003'))).getMonths());
-    assert.equal(24,  (TS.createBubble(60, 2012, TS.parseDate('2002'), TS.parseDate('2004'))).getMonths());
-
-    assert.equal(9,   (TS.createBubble(60, 2012, TS.parseDate('04/2002'), TS.parseDate('2002'))).getMonths());
-    assert.equal(9,   (TS.createBubble(60, 2012, TS.parseDate('04/2002'), TS.parseDate('2003'))).getMonths());
-    assert.equal(21,  (TS.createBubble(60, 2012, TS.parseDate('04/2002'), TS.parseDate('2004'))).getMonths());
-
-    assert.equal(13,  (TS.createBubble(60, 2012, TS.parseDate('04/2002'), TS.parseDate('04/2003'))).getMonths());
-
-    assert.equal(25,  (TS.createBubble(60, 2012, TS.parseDate('04/2002'), TS.parseDate('04/2004'))).getMonths());
-
-    assert.equal(1,   (TS.createBubble(60, 2012, TS.parseDate('04/2002'))).getMonths());
-    assert.equal(12,  (TS.createBubble(60, 2012, TS.parseDate('2002'))).getMonths());
+    assert.equal(29,  (TS.createBubble(60, new Date(2012, 0, 1), new Date(2012, 0, 1), new Date(2012, 0, 30))).getDays());
 
     done();
   });
